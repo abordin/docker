832b920b2f6dc78e576780d803f2b8f3f153c686
Jackson Veroneze <jackson@jacksonveroneze.com>
2017-06-19 09:44:23 -0300

    Ajustes. [ci skip]

--- variables-ambient.sh
+++ variables-ambient.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DIR_SSH=$HOME/.ssh/
+export DIR_SSH=$HOME/.ssh2/
 export DIR_COMPOSER=$HOME/.composer/
 export DIR_DATA_STORAGE_E_SCHOOL=$HOME/.datastorage/mysql/e_school
 export DIR_DATA_STORAGE_GMAS=$HOME/.datastorage/postgres/gmas
@@ -11,3 +11,5 @@ export DIR_TMP_GMUS=$HOME/.tmp/gmus
 export DIR_PGADMIN_DATA=$HOME/.tmp/pgadmin
 
 export PASSWORD_STORAGE=0ac03851e944231e8b05e553dc6637f8
+
+bash
