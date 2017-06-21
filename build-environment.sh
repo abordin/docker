3987fd1d2258f2b45e2e5a6ca9d7c3c17b6399ff
Jackson Veroneze <jackson@jacksonveroneze.com>
2017-06-21 10:53:15 -0300

    Ajustes. [ci skip]

---
+++ build-environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash		
+		
+source ./environment-variables.sh	
+		
+mkdir -p $DIR_SSH		
+mkdir -p $DIR_COMPOSER		
+mkdir -p $DIR_DATA_STORAGE_E_SCHOOL		
+mkdir -p $DIR_DATA_STORAGE_GMAS		
+mkdir -p $DIR_DATA_STORAGE_GMUS		
+mkdir -p $DIR_TMP_E_SCHOOL		
+mkdir -p $DIR_TMP_GMAS		
+mkdir -p $DIR_TMP_GMUS		
+mkdir -p $DIR_PGADMIN_DATA 
---
+++ environment-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash		
+		
+export DIR_SSH=$HOME/.ssh/		
+export DIR_COMPOSER=$HOME/.composer/		
+export DIR_DATA_STORAGE_E_SCHOOL=$HOME/.datastorage/mysql/e_school		
+export DIR_DATA_STORAGE_GMAS=$HOME/.datastorage/postgres/gmas		
+export DIR_DATA_STORAGE_GMUS=$HOME/.datastorage/postgres/gmus		
+export DIR_TMP_E_SCHOOL=$HOME/.tmp/e_school		
+export DIR_TMP_GMAS=$HOME/.tmp/gmas		
+export DIR_TMP_GMUS=$HOME/.tmp/gmus		
+export DIR_PGADMIN_DATA=$HOME/.tmp/pgadmin		
+		
+export PASSWORD_STORAGE=0ac03851e944231e8b05e553dc6637f8
