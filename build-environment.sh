fe823d9a5b0376522482e16ff30680a4a7d6bb94
Jackson Veroneze <jackson@jacksonveroneze.com>
2017-06-21 15:05:24 -0300

    Ajustes. [ci skip]

--- build-environment.sh
+++ build-environment.sh
@@ -1,13 +1,11 @@
 #!/bin/bash		
 		
-source ./environment-variables.sh	
-		
-mkdir -p $DIR_SSH		
-mkdir -p $DIR_COMPOSER		
-mkdir -p $DIR_DATA_STORAGE_E_SCHOOL		
-mkdir -p $DIR_DATA_STORAGE_GMAS		
-mkdir -p $DIR_DATA_STORAGE_GMUS		
-mkdir -p $DIR_TMP_E_SCHOOL		
-mkdir -p $DIR_TMP_GMAS		
-mkdir -p $DIR_TMP_GMUS		
-mkdir -p $DIR_PGADMIN_DATA 
+source ./environment-variables.sh
+
+mkdir -p $DIR_SSH
+mkdir -p $DIR_COMPOSER
+mkdir -p $DIR_TMP
+mkdir -p $DIR_PGADMIN_DATA
+mkdir -p $DIR_DATA_STORAGE_E_SCHOOL
+mkdir -p $DIR_DATA_STORAGE_GMAS
+mkdir -p $DIR_DATA_STORAGE_GMUS
--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,8 @@
-#!/bin/bash		
-		
-cd php7.0-fpm-nginx 		
-&& ./docker-build.sh \		
-&& cd ../php7.1-fpm-nginx \		
+#!/bin/bash
+
+cd php7.0-fpm-nginx
+&& ./docker-build.sh \
+&& cd ../php7.1-fpm-nginx \
 && ./docker-build.sh		
-&& cd ../postgres9.4-ssl \		
-&& ./docker-build.sh 
+&& cd ../postgres9.4-ssl \
+&& ./docker-build.sh
