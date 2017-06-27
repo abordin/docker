7fb34a921706036ec04f335a03971e7de4bcc1a2
Jackson Veroneze <jackson@jacksonveroneze.com>
2017-06-27 13:16:53 -0300

    Ajustes. [ci skip]

--- build-images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd php7.0-fpm-nginx
-&& ./docker-build.sh \
-&& cd ../php7.1-fpm-nginx \
-&& ./docker-build.sh		
-&& cd ../postgres9.4-ssl \
-&& ./docker-build.sh
--- postgres9.4-ssl/ssl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo hostssl\ all\ all\ 0.0.0.0/0\ md5 >> $PGDATA/pg_hba.conf
-cat /etc/postgresql-common/ssl.conf >> $PGDATA/postgresql.conf
-sed -i "s/datestyle = 'iso, dmy'/datestyle = 'iso, DMY'/" $PGDATA/postgresql.conf
