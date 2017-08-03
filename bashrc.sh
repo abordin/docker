6dae275704b48c2e130ebebf2b3ca84d6fcee274
Jackson Veroneze <jackson@jacksonveroneze.com>
2017-08-03 08:03:27 -0300

    Ajustes.

--- bashrc.sh
+++ bashrc.sh
@@ -4,12 +4,12 @@ source /home/$USER/www/docker/environment-variables.sh
 #
 alias docker-clear='docker rm -f `docker ps -aq`'
 #
-alias docker-stats="docker stats $(docker ps|grep -v 'NAMES'|awk '{ print $NF }'|tr '\n' ' ')"
-#
 alias docker-clear-images='docker rmi $(docker images -f "dangling=true" -q)'
 #
 alias docker-gmus='docker-compose -f /home/$USER/www/docker/yml/docker-compose-gmus.yml -p gmus up -d'
 alias docker-gmas='docker-compose -f /home/$USER/www/docker/yml/docker-compose-gmas.yml -p gmas up -d'
+#
+alias docker-gmas-api='docker-compose -f /home/$USER/www/docker/yml/docker-compose-gmas-api.yml -p gmas-api up -d'
 #
 alias docker-gmus-crypt='docker-compose -f /home/$USER/www/docker/yml/docker-compose-gmus-crypt.yml -p gmus-crypt up -d'
 alias docker-gmas-crypt='docker-compose -f /home/$USER/www/docker/yml/docker-compose-gmas-crypt.yml -p gmas-crypt up -d'
